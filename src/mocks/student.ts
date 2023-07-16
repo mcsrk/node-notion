36a9d67f686b63449be4d71ac1e62b19a31f8bfe
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-07-16 00:47:03 -0500

    redefined student mock data

--- src/mocks/student.ts
+++ src/mocks/student.ts
@@ -1,57 +1,57 @@
 import { SUBJECTS } from '../constants/subjects';
 
-export const demoStudentData = {
+export const DEMO_STUDENT_DATA = Object.freeze({
 	user: {
 		name: 'Andres Carne de Res',
 		objectId: 'userID54321',
 		email: 'andres@gmail.com',
 	},
-	grades: {
+	grade: {
 		name: 'Year 5 Selective',
 		overall: 0.84,
 		subjects: [
 			{
-				name: SUBJECTS.MATH,
+				name: SUBJECTS.MATH.NAME,
 				performance: 0.1,
 				topicsOfDifficulty: [
-					{ name: 'Poetry', performance: 0.11 },
-					{ name: 'Comparative Text', performance: 0.12 },
+					{ name: 'Poetry - Replaced from server', performance: 0.11 },
+					{ name: 'Comparative Text - Replaced from server', performance: 0.12 },
 					{ name: 'Cloze Passage', performance: 0.13 },
 				],
 				skillsOfDifficulty: [
-					{ name: 'Comprehension ', performance: 0.14 },
-					{ name: 'Textual Analysis', performance: 0.15 },
-					{ name: 'Contextual Understanding', performance: 0.16 },
+					{ name: 'Comprehension  - Replaced from server', performance: 0.14 },
+					{ name: 'Textual Analysis - Replaced from server', performance: 0.15 },
+					{ name: 'Contextual Understanding - Replaced from server', performance: 0.16 },
 				],
 			},
 			{
-				name: SUBJECTS.READING,
+				name: SUBJECTS.READING.NAME,
 				performance: 0.2,
 				topicsOfDifficulty: [
-					{ name: 'Algebra ', performance: 0.21 },
-					{ name: 'Fractions ', performance: 0.22 },
-					{ name: 'Word Problems', performance: 0.23 },
+					{ name: 'Algebra  - Replaced from server', performance: 0.21 },
+					{ name: 'Fractions  - Replaced from server', performance: 0.22 },
+					{ name: 'Word Problems - Replaced from server', performance: 0.23 },
 				],
 				skillsOfDifficulty: [
-					{ name: 'Numerical operations', performance: 0.24 },
-					{ name: 'Problem-solving', performance: 0.25 },
-					{ name: 'Logic and reasoning', performance: 0.26 },
+					{ name: 'Numerical operations - Replaced from server', performance: 0.24 },
+					{ name: 'Problem-solving - Replaced from server', performance: 0.25 },
+					{ name: 'Logic and reasoning - Replaced from server', performance: 0.26 },
 				],
 			},
 			{
-				name: SUBJECTS.THINKING,
+				name: SUBJECTS.THINKING.NAME,
 				performance: 0.3,
 				topicsOfDifficulty: [
-					{ name: 'Dialogue Flaws', performance: 0.31 },
-					{ name: 'Strengthen & Weaken', performance: 0.32 },
-					{ name: 'Logic Games', performance: 0.33 },
+					{ name: 'Dialogue Flaws - Replaced from server', performance: 0.31 },
+					{ name: 'Strengthen & Weaken - Replaced from server', performance: 0.32 },
+					{ name: 'Logic Games - Replaced from server', performance: 0.33 },
 				],
 				skillsOfDifficulty: [
-					{ name: 'Analytical Thinking', performance: 0.34 },
-					{ name: 'Textual Analysis', performance: 0.35 },
-					{ name: 'Contextual Understanding', performance: 0.36 },
+					{ name: 'Analytical Thinking - Replaced from server', performance: 0.34 },
+					{ name: 'Textual Analysis - Replaced from server', performance: 0.35 },
+					{ name: 'Contextual Understanding - Replaced from server', performance: 0.36 },
 				],
 			},
 		],
 	},
-};
+});
