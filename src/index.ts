6a00590340d190125e1808379892009122b404d2
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-07-16 00:52:32 -0500

    improved server instance

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,29 @@ import roadmapRoutes from './routes/roadmap.routes';
 
 const app = express();
 
-app.use('/notion', notionRoute);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('Notion roadmap manipulator - Scholary');
+app.use((req, res, next) => {
+	/** Requests from anywhere */
+	res.header('Access-Control-Allow-Origin', '*');
+	/** Headers allowed to use */
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	/** All of the options that can be used in this API */
+	if (req.method == 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+		return res.status(200).json({});
+	}
+
+	next();
+});
+
+app.get('/', (_, res) => {
+	res.send('Notion roadmap microservice - Scholary');
 });
 
 app.use('/students', roadmapRoutes);
+
 const PORT = 8000;
 
 app.listen(PORT, () => {
