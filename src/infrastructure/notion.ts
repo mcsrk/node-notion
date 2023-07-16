8f1ef24381ae633f625f9789afbe3f19ac1892e1
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-07-16 00:55:41 -0500

    implemented create studentid page

--- src/infrastructure/notion.ts
+++ src/infrastructure/notion.ts
@@ -64,6 +64,55 @@ export class NotionClient {
 		return result;
 	}
 
+	async createStudentIdPage(studentId: string) {
+		const searchStudentIdPageRes = await this.searchPage(studentId);
+
+		const existingStudentIdPage = searchStudentIdPageRes.results[0];
+		if (existingStudentIdPage) {
+			/** This code deletes the code of a existing page, which takes a lot of time (3 mins per page/100 blocks) */
+			const blocksToRemove = await this.notion.blocks.children.list({
+				block_id: existingStudentIdPage.id,
+				page_size: CONFIG.ROADMAP_TEMPLATE.OVERRIDE_BLOCKS,
+			});
+			console.log(
+				`Student Id Page '${studentId}' exists already (${existingStudentIdPage.id}), removing ${blocksToRemove.results.length} blocks...`,
+			);
+			for (const block of blocksToRemove.results) {
+				await this.notion.blocks.delete({ block_id: block.id });
+			}
+
+			return existingStudentIdPage.id;
+		}
+		const studentCollectionName = CONFIG.ROADMAP_TEMPLATE.STUDENTS_COLLECTION_PAGE_NAME;
+		const studentCollectionPageRes = await this.searchPage(studentCollectionName);
+
+		const existingStudentCollectionPage = studentCollectionPageRes.results[0];
+		if (!existingStudentCollectionPage) {
+			throw new Error(`No student collections found by name ${studentCollectionName}`);
+		}
+
+		const studentIdPageBody: CreatePageParameters = {
+			parent: { page_id: existingStudentCollectionPage.id, type: 'page_id' },
+			icon: {
+				emoji: '👤',
+			},
+			properties: {
+				title: {
+					title: [
+						{
+							text: {
+								content: studentId,
+							},
+						},
+					],
+				},
+			},
+		};
+
+		const newStudentIdPage = await this.createPage(studentIdPageBody);
+		return newStudentIdPage.id;
+	}
+
 	async archivePage(pageId: string): Promise<UpdatePageResponse> {
 		const updatePageResponse = await this.notion.pages.update({
 			page_id: pageId,
