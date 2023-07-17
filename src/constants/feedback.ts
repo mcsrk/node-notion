7820e7422956c2d20340590768bf1a6d0645686a
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-07-17 01:01:09 -0500

    defined default feedback messages

---
+++ src/constants/feedback.ts
@@ -0,0 +1,8 @@
+/** Maps a subject and its respective preffix
+ *  used in every subject-related variable in
+ *  the Notion Roadmap Template */
+
+export const DEFAULT_FEEDBACK_MSG = Object.freeze({
+	SPECIAL_COMMENT: 'No special comments defined yet',
+	IMPROVEMENT_STRATEGY: '-',
+});
